9c7fac947242500a77f441419860397a8e18d5bb
judy99 <julia.kurianova@gmail.com>
2025-03-20 21:12:35 -0400

    add notification service

--- 03-di/01-notification-service/tasks/tasks.service.ts
+++ 03-di/01-notification-service/tasks/tasks.service.ts
@@ -1,11 +1,20 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from "@nestjs/common";
 import { CreateTaskDto, Task, TaskStatus, UpdateTaskDto } from "./task.model";
+import { NotificationsService } from "../notifications/notifications.service";
+import { UsersService } from "../users/users.service";
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
 
-  constructor() {}
+  constructor(
+    private readonly notificationsService: NotificationsService,
+    private readonly usersService: UsersService,
+  ) {}
 
   async createTask(createTaskDto: CreateTaskDto) {
     const { title, description, assignedTo } = createTaskDto;
@@ -16,7 +25,28 @@ export class TasksService {
       status: TaskStatus.Pending,
       assignedTo,
     };
+
     this.tasks.push(task);
+
+    // get user
+    const user = this.usersService.getUserById(assignedTo);
+    if (!user) {
+      throw new NotFoundException(`Пользователь с ID ${assignedTo} не найден`);
+    }
+
+    // empty user email or task title
+    if (!user.email || !title) {
+      throw new BadRequestException(
+        `The user email or task title cannot be empty.`,
+      );
+    }
+
+    // send email notification
+    this.notificationsService.sendEmail(
+      user.email,
+      "Новая задача",
+      `Вы назначены ответственным за задачу: "${task.title}"`,
+    );
 
     return task;
   }
@@ -28,6 +58,26 @@ export class TasksService {
     }
 
     Object.assign(task, updateTaskDto);
+
+    // get user
+    const user = this.usersService.getUserById(task.assignedTo);
+    if (!user) {
+      throw new NotFoundException(
+        `Пользователь с ID ${task.assignedTo} не найден`,
+      );
+    }
+
+    // empty user phone
+    if (!user.phone) {
+      throw new BadRequestException(`The user phone cannot be empty.`);
+    }
+
+    // send sms notification
+    this.notificationsService.sendSMS(
+      user.phone,
+      `Статус задачи "${task.title}" обновлён на "${task.status}"`,
+    );
+
     return task;
   }
 }
