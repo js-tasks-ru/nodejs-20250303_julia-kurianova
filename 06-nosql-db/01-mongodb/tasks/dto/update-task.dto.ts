1a73bb1e4a153f91a293ce39757ff209a87f699a
judy99 <julia.kurianova@gmail.com>
2025-04-07 11:48:01 -0400

    remove unnecessary code

--- 06-nosql-db/01-mongodb/tasks/dto/update-task.dto.ts
+++ 06-nosql-db/01-mongodb/tasks/dto/update-task.dto.ts
@@ -2,14 +2,4 @@ import { PartialType } from "@nestjs/mapped-types";
 import { CreateTaskDto } from "./create-task.dto";
 import { IsBoolean, IsNotEmpty, IsString } from "class-validator";
 
-export class UpdateTaskDto extends PartialType(CreateTaskDto) {
-  @IsString()
-  @IsNotEmpty()
-  title: string;
-
-  @IsString()
-  description: string;
-
-  @IsBoolean()
-  isCompleted: boolean;
-}
+export class UpdateTaskDto extends PartialType(CreateTaskDto) {}
