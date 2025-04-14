72b01aba3c56a34fa2437dd709869ead49b471de
Julia Kurianova <julia.kurianova@gmail.com>
2025-04-14 13:40:37 -0400

    Update jwt.ts

--- 07-authentication-authorization/01-google-auth/config/jwt.ts
+++ 07-authentication-authorization/01-google-auth/config/jwt.ts
@@ -1,7 +1,7 @@
 import { registerAs } from "@nestjs/config";
 
 export default registerAs("jwt", () => ({
-  secret: process.env.JWT_SECRET,
+  secret: process.env.JWT_SECRET || "killer-is-jim",
   signOptions: { expiresIn: "20s" },
   refreshTokenExpires: "1h",
 }));
