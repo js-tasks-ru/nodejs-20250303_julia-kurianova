07ba1127ac46e7b4c7654440056f3b2f41bb2638
judy99 <julia.kurianova@gmail.com>
2025-04-14 12:57:12 -0400

    Module 7 - auth

--- 07-authentication-authorization/01-google-auth/auth/auth.module.ts
+++ 07-authentication-authorization/01-google-auth/auth/auth.module.ts
@@ -2,10 +2,30 @@ import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { UsersModule } from "../users/users.module";
+import { JwtStrategy } from "./passport/jwt.strategy";
+import { GoogleStrategy } from "./passport/google.strategy";
+import { JwtModule } from "@nestjs/jwt";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { PassportModule } from "@nestjs/passport";
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule,
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.get("jwt.secret"),
+          signOptions: configService.get("jwt.signOptions"),
+          global: true,
+        };
+      },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy, GoogleStrategy],
 })
 export class AuthModule {}
